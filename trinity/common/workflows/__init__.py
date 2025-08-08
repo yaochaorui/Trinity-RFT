fb2db4f58818d99aae8234c9469f486677d620d8
garyzhang99 <46197280+garyzhang99@users.noreply.github.com>
2025-08-08 17:40:59 +0800

    Add agentscope react multi-turn toolcalls example (#165)

--- trinity/common/workflows/__init__.py
+++ trinity/common/workflows/__init__.py
@@ -2,6 +2,7 @@
 """Workflow module"""
 from .customized_math_workflows import MathBoxedWorkflow
 from .customized_toolcall_workflows import ToolCallWorkflow
+from .envs.agentscope.agentscope_react_workflow import AgentScopeReactV2MathWorkflow
 from .envs.alfworld.alfworld_workflow import AlfworldWorkflow
 from .envs.sciworld.sciworld_workflow import SciWorldWorkflow
 from .envs.webshop.webshop_workflow import WebShopWorkflow
@@ -22,4 +23,5 @@ __all__ = [
     "MathRMWorkflow",
     "ToolCallWorkflow",
     "MathEvalWorkflow",
+    "AgentScopeReactV2MathWorkflow",
 ]
