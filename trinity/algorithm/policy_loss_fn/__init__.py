81c7768ab36379ae83be1694804f2806b6fea620
cr <chaorui@ucla.edu>
2025-08-28 14:50:40 +0800

    [algo] Implemented sPPO.

--- trinity/algorithm/policy_loss_fn/__init__.py
+++ trinity/algorithm/policy_loss_fn/__init__.py
@@ -10,6 +10,7 @@ from trinity.algorithm.policy_loss_fn.opmd_policy_loss import OPMDPolicyLossFn
 from trinity.algorithm.policy_loss_fn.policy_loss_fn import POLICY_LOSS_FN, PolicyLossFn
 from trinity.algorithm.policy_loss_fn.ppo_policy_loss import PPOPolicyLossFn
 from trinity.algorithm.policy_loss_fn.sft_loss import SFTLossFn
+from trinity.algorithm.policy_loss_fn.sppo_loss_fn import sPPOPolicyLossFn
 
 __all__ = [
     "POLICY_LOSS_FN",
@@ -23,4 +24,5 @@ __all__ = [
     "MIXCHORDPolicyLossFn",
     "SFTISLossFn",
     "SFTPhiLossFn",
+    "sPPOPolicyLossFn",
 ]
